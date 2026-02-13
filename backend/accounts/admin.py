dfa6e4d870ccdc9eb09412b153a4d6410b5b82d6
shadiyashirin <vshadiyashirin2003@gmail.com>
2026-02-13 11:17:55 +0530

    'initial-user-model-setup'

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ("username", "email", "is_staff", "is_active")
